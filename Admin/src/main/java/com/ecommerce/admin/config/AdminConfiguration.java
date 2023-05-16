e15665099e6f48132508274f1beddc4d388b4eb3
phamnhatkhanh <khanhhcm4@gmail.com>
2023-05-16 16:19:53 +0700

    Done: Login and Register

--- Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
+++ Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
@@ -1,5 +1,6 @@
 package com.ecommerce.admin.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -28,6 +29,7 @@ public class AdminConfiguration {
     }
 
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder
@@ -41,7 +43,8 @@ public class AdminConfiguration {
 
         http
                 .authorizeHttpRequests()
-                .requestMatchers("/*", "/static/**").permitAll()
+                .requestMatchers("/*").permitAll()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
